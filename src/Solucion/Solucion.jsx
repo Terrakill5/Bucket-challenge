31c2d259a8a64e495b69b55958cb45548cdc4902
JBriceno <josealbertino4@gmail.com>
2023-05-06 14:08:23 -0500

    Creadas las funciones de los buckets, y organizado mejor el codigo

--- src/Solucion/Solucion.jsx
+++ src/Solucion/Solucion.jsx
@@ -40,12 +40,7 @@ const Solucion = (bucketX) => {
         // Aqui se busca la mejor solucion, para ver de que lado se puede llegar mas rapido
         //En el caso siguiente, demuestra que X esta mas lejos que Y, por lo que iremos reduciendo Y desde su maximo
         if (primerValor > segundoValor) {
-          explicacionSolucion.push({
-            bucket: 0,
-            bucketY: bucketX.y,
-            explanation: "Fill bucketY",
-          });
-
+          llenarBalde(explicacionSolucion, "y");
           while (
             bucketX.medida !==
             explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
@@ -54,58 +49,39 @@ const Solucion = (bucketX) => {
               explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
             );
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: bucketX.x,
-              bucketY: explicacionSolucion[i - 1].bucketY - bucketX.x,
-              explanation: "Transfer bucketY to bucketX",
-            });
+            transferirBalde(
+              explicacionSolucion,
+              explicacionSolucion[i - 1].bucketY,
+              explicacionSolucion[i - 1].bucket
+            );
+
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: 0,
-              bucketY: explicacionSolucion[i - 1].bucketY,
-              explanation: "Dump bucketX",
-            });
-            console.log("antes de terminar el while", i, explicacionSolucion);
-            if (i > 10) {
-              return;
-            }
+            vaciarBalde(explicacionSolucion, "x");
           }
         } else {
           // en este caso, se lleva a Y vacio y se va incrementando dado que el valor esta mas cerca de X
           console.log("entrada alternada");
+          llenarBalde(explicacionSolucion, "x");
 
-          explicacionSolucion.push({
-            bucket: bucketX.x,
-            bucketY: 0,
-            explanation: "Fill bucket bucketX",
-          });
           while (
             bucketX.medida !==
             explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
           ) {
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: 0,
-              bucketY: bucketX.x + explicacionSolucion[i - 1].bucketY,
-              explanation: "Transfer bucketX to  bucketY",
-            });
+            transferirBalde(
+              explicacionSolucion,
+              explicacionSolucion[i - 1].bucket,
+              explicacionSolucion[i - 1].bucketY
+            );
 
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: bucketX.x,
-              bucketY: explicacionSolucion[i - 1].bucketY,
-              explanation: "Fill bucket x",
-            });
+            llenarBalde(explicacionSolucion, "x");
           }
         }
       } else {
         //En este caso es cuando X es mayor que Y, por lo que se hace lo contrario al caso anterior
         if (primerValor > segundoValor) {
-          explicacionSolucion.push({
-            bucket: 0,
-            bucketY: bucketX.y,
-            explanation: "Fill bucketY",
-          });
+          llenarBalde(explicacionSolucion, "y");
 
           while (
             bucketX.medida !==
@@ -115,17 +91,15 @@ const Solucion = (bucketX) => {
               explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
             );
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: explicacionSolucion[i - 1].bucket + bucketX.y,
-              bucketY: 0,
-              explanation: "Transfer bucketY to bucketX",
-            });
+            transferirBalde(
+              explicacionSolucion,
+              explicacionSolucion[i - 1].bucketY,
+              explicacionSolucion[i - 1].bucket
+            );
+
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: explicacionSolucion[i - 1].bucket,
-              bucketY: bucketX.y,
-              explanation: "Fill bucketY",
-            });
+            llenarBalde(explicacionSolucion, "y");
+
             console.log("antes de terminar el while", i, explicacionSolucion);
             if (i > 10) {
               return;
@@ -134,29 +108,21 @@ const Solucion = (bucketX) => {
         } else {
           // en este caso, se lleva a X lleno y se va reduciendo dado que el valor esta mas cerca que Y
           console.log("entrada alternada");
+          llenarBalde(explicacionSolucion, "x");
 
-          explicacionSolucion.push({
-            bucket: bucketX.x,
-            bucketY: 0,
-            explanation: "Fill bucket bucketX",
-          });
           while (
             bucketX.medida !==
             explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
           ) {
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: explicacionSolucion[i - 1].bucket - bucketX.y,
-              bucketY: bucketX.y,
-              explanation: "Transfer bucketX to  bucketY",
-            });
+            transferirBalde(
+              explicacionSolucion,
+              explicacionSolucion[i - 1].bucket,
+              explicacionSolucion[i - 1].bucketY
+            );
 
             i = i + 1;
-            explicacionSolucion.push({
-              bucket: explicacionSolucion[i - 1].bucket,
-              bucketY: 0,
-              explanation: "Dumb bucket y",
-            });
+            vaciarBalde(explicacionSolucion, "y");
           }
           console.log("llego a esta solucion");
         }
@@ -168,32 +134,114 @@ const Solucion = (bucketX) => {
     }
     console.log("resultado", explicacionSolucion);
   };
+
+  const vaciarBalde = (arraySolucion, balde) => {
+    let posicion = arraySolucion.length - 1;
+    if (posicion < 0) {
+      posicion = 0;
+    }
+    if (balde === "x") {
+      arraySolucion.push({
+        bucket: 0,
+        bucketY: arraySolucion[posicion].bucketY,
+        explanation: "Dumb bucketX",
+      });
+    } else {
+      arraySolucion.push({
+        bucket: arraySolucion[posicion].bucket,
+        bucketY: 0,
+        explanation: "Dumb bucketY",
+      });
+    }
+  };
+  const llenarBalde = (arraySolucion, balde) => {
+    if (balde === "x") {
+      // Si el array esta en la primera posicion, no tendra valores anteriores, por lo que el balde que
+      // no se esta llenando es 0, inicialmente
+      if (arraySolucion.length - 1 < 0) {
+        arraySolucion.push({
+          bucket: bucketX.x,
+          bucketY: 0,
+          explanation: "Filled bucketX",
+        });
+      } else {
+        arraySolucion.push({
+          bucket: bucketX.x,
+          bucketY: arraySolucion[arraySolucion.length - 1].bucketY,
+          explanation: "Filled bucketX",
+        });
+      }
+    } else {
+      // Si el array esta en la primera posicion, no tendra valores anteriores, por lo que el balde que
+      // no se esta llenando es 0, inicialmente
+      if (arraySolucion.length - 1 < 0) {
+        arraySolucion.push({
+          bucket: 0,
+          bucketY: bucketX.y,
+          explanation: "Filled bucketY",
+        });
+      } else {
+        arraySolucion.push({
+          bucket: arraySolucion[arraySolucion.length - 1].bucket,
+          bucketY: bucketX.y,
+          explanation: "Filled bucketY",
+        });
+      }
+    }
+  };
+  const transferirBalde = (arraySolucion, baldeDesde, baldeHasta) => {
+    // Aqui se busca cual es el balde que va a transferir todo su contenido
+    let cantidad = 0;
+    if (arraySolucion[arraySolucion.length - 1].bucketY === baldeDesde) {
+      // una vez encontrado va a pasar todo su contenido al otro balde, aca se decide la cantidad a transferir
+
+      if (baldeDesde > baldeHasta) {
+        cantidad = bucketX.x;
+      } else {
+        cantidad = bucketX.y;
+      }
+      // finalmente dependiendo del contenido maximo, se asignan los valores
+      arraySolucion.push({
+        bucket: arraySolucion[arraySolucion.length - 1].bucket + cantidad,
+        bucketY: arraySolucion[arraySolucion.length - 1].bucketY - cantidad,
+        explanation: "Transfer bucketY to  bucketX",
+      });
+    } else {
+      if (baldeDesde > baldeHasta) {
+        cantidad = bucketX.y;
+      } else {
+        cantidad = bucketX.x;
+      }
+      arraySolucion.push({
+        bucket: arraySolucion[arraySolucion.length - 1].bucket - cantidad,
+        bucketY: arraySolucion[arraySolucion.length - 1].bucketY + cantidad,
+        explanation: "Transfer bucketX to  bucketY",
+      });
+    }
+  };
   useEffect(() => {
     console.log("entro al useEffect");
     explicacion();
   }, [bucketX.trigger]);
   return (
-    <>
-      <Table responsive bordered>
-        <thead>
-          <tr>
-            <th className="tg-0labucketX tebucketXt-center">BucketX</th>
-            <th className="tg-0labucketX tebucketXt-center">BucketY</th>
-            <th className="tg-0labucketX tebucketXt-center ">explanation</th>
+    <Table responsive bordered>
+      <thead>
+        <tr>
+          <th className="tg-0labucketX tebucketXt-center">BucketX</th>
+          <th className="tg-0labucketX tebucketXt-center">BucketY</th>
+          <th className="tg-0labucketX tebucketXt-center ">explanation</th>
+        </tr>
+      </thead>
+      <tbody>
+        {explicacionSolucion.map((item, index) => (
+          <tr key={`ctcns${index}`}>
+            <td>{item.bucket}</td>
+            <td>{item.bucketY}</td>
+            <td>{item.explanation}</td>
           </tr>
-        </thead>
-        <tbody>
-          {explicacionSolucion.length > 0 &&
-            explicacionSolucion.map((item, index) => (
-              <tr key={`ctcns${index}`}>
-                <td>{item.bucket}</td>
-                <td>{item.bucketY}</td>
-                <td>{item.explanation}</td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
-    </>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
