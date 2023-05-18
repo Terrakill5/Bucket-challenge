66eccba4b8c5b00c1e83abf5ba88f080534bc52b
JBriceno <josealbertino4@gmail.com>
2023-05-17 19:03:34 -0500

    Se ha refactorizado las funciones

--- src/Solucion/Solucion.jsx
+++ src/Solucion/Solucion.jsx
@@ -20,8 +20,14 @@ const Solucion = (bucketX) => {
     // Entra al primer caso, el mas sencillo, donde los valores de X y Y ya son la medida
 
     if (bucketX.medida === bucketX.x + bucketX.y) {
-      llenarBalde(explicacionSolucion, "x");
-      llenarBalde(explicacionSolucion, "y");
+      llenarBalde(explicacionSolucion, "bucket", bucketX.x, "bucketY", 0);
+      llenarBalde(
+        explicacionSolucion,
+        "bucketY",
+        bucketX.y,
+        "bucket",
+        bucketX.x
+      );
     } else {
       let primerValor = bucketX.medida - bucketX.x;
       if (primerValor < 0) primerValor = primerValor * -1;
@@ -36,83 +42,116 @@ const Solucion = (bucketX) => {
         //En el caso siguiente, demuestra que X esta mas lejos que Y, por lo que iremos reduciendo Y desde su maximo
 
         if (primerValor > segundoValor) {
-          llenarBalde(explicacionSolucion, "y");
+          llenarBalde(explicacionSolucion, "bucketY", bucketX.y, "bucket", 0);
           while (
             bucketX.medida !==
             explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
           ) {
             i = i + 1;
+
             transferirBalde(
               explicacionSolucion,
-              explicacionSolucion[i - 1].bucketY,
-              explicacionSolucion[i - 1].bucket
-            );
-
-            i = i + 1;
-            vaciarBalde(explicacionSolucion, "x");
-          }
-        } else {
-          // en este caso, se lleva a Y vacio y se va incrementando dado que el valor esta mas cerca de X
-
-          llenarBalde(explicacionSolucion, "x");
-
-          while (
-            bucketX.medida !==
-            explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
-          ) {
-            i = i + 1;
-            transferirBalde(
-              explicacionSolucion,
+              "bucket",
+              "bucketY",
+              bucketX.x,
               explicacionSolucion[i - 1].bucket,
               explicacionSolucion[i - 1].bucketY
             );
 
             i = i + 1;
-            llenarBalde(explicacionSolucion, "x");
+            vaciarBalde(
+              explicacionSolucion,
+              "bucket",
+              "bucketY",
+              explicacionSolucion[i - 1].bucketY
+            );
+          }
+        } else {
+          // en este caso, se lleva a Y vacio y se va incrementando dado que el valor esta mas cerca de X
+
+          llenarBalde(explicacionSolucion, "bucket", bucketX.x, "bucketY", 0);
+
+          while (
+            bucketX.medida !==
+            explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
+          ) {
+            i = i + 1;
+
+            transferirBalde(
+              explicacionSolucion,
+              "bucketY",
+              "bucket",
+              bucketX.x,
+              explicacionSolucion[i - 1].bucketY,
+              explicacionSolucion[i - 1].bucket
+            );
+
+            i = i + 1;
+            llenarBalde(
+              explicacionSolucion,
+              "bucket",
+              bucketX.x,
+              "bucketY",
+              explicacionSolucion[i - 1].bucketY
+            );
           }
         }
       } else {
         //En este caso es cuando X es mayor que Y, por lo que se hace lo contrario al caso anterior
 
         if (primerValor > segundoValor) {
-          llenarBalde(explicacionSolucion, "y");
+          llenarBalde(explicacionSolucion, "bucketY", bucketX.y, "bucket", 0);
 
           while (
             bucketX.medida !==
             explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
           ) {
             i = i + 1;
+
             transferirBalde(
               explicacionSolucion,
-              explicacionSolucion[i - 1].bucketY,
-              explicacionSolucion[i - 1].bucket
-            );
-
-            i = i + 1;
-            llenarBalde(explicacionSolucion, "y");
-
-            if (i > 10) {
-              return;
-            }
-          }
-        } else {
-          // en este caso, se lleva a X lleno y se va reduciendo dado que el valor esta mas cerca que Y
-
-          llenarBalde(explicacionSolucion, "x");
-
-          while (
-            bucketX.medida !==
-            explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
-          ) {
-            i = i + 1;
-            transferirBalde(
-              explicacionSolucion,
+              "bucket",
+              "bucketY",
+              bucketX.y,
               explicacionSolucion[i - 1].bucket,
               explicacionSolucion[i - 1].bucketY
             );
 
             i = i + 1;
-            vaciarBalde(explicacionSolucion, "y");
+            llenarBalde(
+              explicacionSolucion,
+              "bucketY",
+              bucketX.y,
+              "bucket",
+              explicacionSolucion[i - 1].bucket
+            );
+          }
+        } else {
+          // en este caso, se lleva a X lleno y se va reduciendo dado que el valor esta mas cerca que Y
+
+          llenarBalde(explicacionSolucion, "bucket", bucketX.x, "bucketY", 0);
+
+          while (
+            bucketX.medida !==
+            explicacionSolucion[i].bucket + explicacionSolucion[i].bucketY
+          ) {
+            i = i + 1;
+            transferirBalde(
+              explicacionSolucion,
+              "bucketY",
+              "bucket",
+              bucketX.y,
+              explicacionSolucion[i - 1].bucketY,
+              explicacionSolucion[i - 1].bucket
+            );
+
+            i = i + 1;
+            vaciarBalde(
+              explicacionSolucion,
+              "bucketY",
+              "bucket",
+              explicacionSolucion[i - 1].bucket
+            );
           }
         }
       }
@@ -128,125 +167,75 @@ const Solucion = (bucketX) => {
   /**
    * Vacía uno de los baldes
    * @param  {Array} arraySolucion array de la solución
-   * @param  {String} balde Selecciona el balde que se quiere vaciar
+   * @param  {String} nombreBaldeVaciar Selecciona el balde que se quiere vaciar
+   * @param  {String} nombreResto Selecciona el balde que va a tener o la posición anterior o inicia en 0
+   * @param  {Number} valorResto Selecciona el valor del balde con su valor inicial o valor de posicion anterior
    * @return {Void}      El objetivo del método es vaciar uno de los baldes, usando una condicional
    */
 
-  const vaciarBalde = (arraySolucion, balde) => {
-    // Primero se selecciona la posición a trabajar
-
-    let posicion = arraySolucion.length - 1;
-
-    // Si la posición es la primera posición se ajusta la variable para no causar error
-
-    if (posicion < 0) {
-      posicion = 0;
-    }
-
-    if (balde === "x") {
-      arraySolucion.push({
-        bucket: 0,
-        bucketY: arraySolucion[posicion].bucketY,
-        explanation: "Dumb bucketX",
-      });
-    } else {
-      arraySolucion.push({
-        bucket: arraySolucion[posicion].bucket,
-        bucketY: 0,
-        explanation: "Dumb bucketY",
-      });
-    }
+  const vaciarBalde = (
+    arraySolucion,
+    nombreBaldeVaciar,
+    nombreResto,
+    valorResto
+  ) => {
+    arraySolucion.push({
+      [nombreBaldeVaciar]: 0,
+      [nombreResto]: valorResto,
+      explanation: `Dumb ${nombreBaldeVaciar}`,
+    });
   };
 
   /**
    * Llena uno de los baldes
    * @param  {Array} arraySolucion array de la solución
-   * @param  {String} balde Selecciona el balde que se quiere llenar
+   * @param  {String} nombreBalde Selecciona el balde que se quiere llenar
+   * @param  {String} nombreResto Selecciona el balde que va a tener o la posición anterior o inicia en 0
+   * @param  {Number} valorBalde Selecciona el valor del balde con que se va a llenar
+   * @param  {Number} valorResto Selecciona el valor del balde con su valor inicial o valor de posicion anterior
    * @return {Void}      El objetivo del método es llenar uno de los baldes, usando condicionales
    */
 
-  const llenarBalde = (arraySolucion, balde) => {
-    if (balde === "x") {
-      // Si el array esta en la primera posicion, no tendra valores anteriores, por lo que el balde que
-      // no se esta llenando es 0, inicialmente
-
-      if (arraySolucion.length - 1 < 0) {
-        arraySolucion.push({
-          bucket: bucketX.x,
-          bucketY: 0,
-          explanation: "Filled bucketX",
-        });
-      } else {
-        // de no ser la primera posición, se le asigna su valor anterior
-        arraySolucion.push({
-          bucket: bucketX.x,
-          bucketY: arraySolucion[arraySolucion.length - 1].bucketY,
-          explanation: "Filled bucketX",
-        });
-      }
-    } else {
-      // Si el array esta en la primera posicion, no tendra valores anteriores, por lo que el balde que
-      // no se esta llenando es 0, inicialmente
-
-      if (arraySolucion.length - 1 < 0) {
-        arraySolucion.push({
-          bucket: 0,
-          bucketY: bucketX.y,
-          explanation: "Filled bucketY",
-        });
-      } else {
-        arraySolucion.push({
-          bucket: arraySolucion[arraySolucion.length - 1].bucket,
-          bucketY: bucketX.y,
-          explanation: "Filled bucketY",
-        });
-      }
-    }
+  const llenarBalde = (
+    arraySolucion,
+    nombreBalde,
+    valorBalde,
+    nombreResto,
+    valorResto
+  ) => {
+    arraySolucion.push({
+      [nombreBalde]: valorBalde,
+      [nombreResto]: valorResto,
+      explanation: `Filled ${nombreBalde}`,
+    });
   };
 
   /**
    * Transfiere uno de los baldes
    * @param  {Array} arraySolucion array de la solución
-   * @param  {Number} baldeDesde Selecciona el balde que se transferir
-   * @param  {Number} baldeHasta Selecciona el balde que recibe la transferencia
+   * @param  {Number} baldeQueTransfiere Selecciona el balde que se va a transferir
+   * @param  {Number} baldeTransferido Selecciona el balde que recibe la transferencia
+   * @param {Number} referencia Valor de referencia que se va a transferir
+   * @param  {Number} valorAnteriorTransferido Selecciona el balde se va a incrementar por la referencia
+   * @param  {Number} valorAnteriorTransfiere Selecciona el balde que va a reducir por la referencia
    * @return {Void}      El objetivo del método es transferir un balde a otro
    */
 
-  const transferirBalde = (arraySolucion, baldeDesde, baldeHasta) => {
-    // Aqui se busca cual es el balde que va a transferir todo su contenido
-
-    let cantidad = 0;
-    if (arraySolucion[arraySolucion.length - 1].bucketY === baldeDesde) {
-      // una vez encontrado va a pasar todo su contenido al otro balde, aca se decide la cantidad a transferir
-
-      if (baldeDesde > baldeHasta) {
-        cantidad = bucketX.x;
-      } else {
-        cantidad = bucketX.y;
-      }
-
-      // finalmente dependiendo del contenido maximo, se asignan los valores
-
-      arraySolucion.push({
-        bucket: arraySolucion[arraySolucion.length - 1].bucket + cantidad,
-        bucketY: arraySolucion[arraySolucion.length - 1].bucketY - cantidad,
-        explanation: "Transfer bucketY to  bucketX",
-      });
-    } else {
-      if (baldeDesde > baldeHasta) {
-        cantidad = bucketX.y;
-      } else {
-        cantidad = bucketX.x;
-      }
-      arraySolucion.push({
-        bucket: arraySolucion[arraySolucion.length - 1].bucket - cantidad,
-        bucketY: arraySolucion[arraySolucion.length - 1].bucketY + cantidad,
-        explanation: "Transfer bucketX to  bucketY",
-      });
-    }
+  const transferirBalde = (
+    arraySolucion,
+    baldeTransferido,
+    baldeQueTransfiere,
+    referencia,
+    valorAnteriorTransferido,
+    valorAnteriorTransfiere
+  ) => {
+    arraySolucion.push({
+      [baldeTransferido]: valorAnteriorTransferido + referencia,
+      [baldeQueTransfiere]: valorAnteriorTransfiere - referencia,
+      explanation: `Transfer ${baldeQueTransfiere} to  ${baldeTransferido} `,
+    });
   };
   useEffect(() => {
-    
     explicacion();
   }, [bucketX.trigger]);
   return (
